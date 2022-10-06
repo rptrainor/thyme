e4da46ccf0ca47d21273eeb1c118df5cfe1eee77
Ryan Trainor <rptrainor@gmail.com>
2022-10-06 14:45:47 -0500

    finished adding tailwindcss and removed unused css modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#193549',
+    },
   },
   plugins: [],
 }
+};
